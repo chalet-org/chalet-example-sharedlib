a49ccb72b5f04db325f2b40a1aae0a4088863360
Andrew King <pepsiotaku@gmail.com>
2021-11-29 13:09:43 -0500

    launch.json, Config.hpp tweaks

--- include/sharedlib/Config.hpp
+++ include/sharedlib/Config.hpp
@@ -3,22 +3,15 @@
 
 #include <sharedlib/Defines.hpp>
 
-#if defined(LIB_WIN32)
-	#if defined(LIB_SHARED)
-		#define LIB_API_EXPORT __declspec(dllexport)
-		#define LIB_API_IMPORT __declspec(dllimport)
-	#else
-        #define LIB_API_EXPORT
-        #define LIB_API_IMPORT
-	#endif
+#if defined(LIB_SHARED) && defined(__GNUC__) && __GNUC__ >= 4
+	#define LIB_API_EXPORT __attribute__((__visibility__("default")))
+	#define LIB_API_IMPORT __attribute__((__visibility__("default")))
+#elif defined(LIB_SHARED) && defined(LIB_WIN32)
+	#define LIB_API_EXPORT __declspec(dllexport)
+	#define LIB_API_IMPORT __declspec(dllimport)
 #else
-	 #if defined(LIB_SHARED) && __GNUC__ >= 4
-        #define LIB_API_EXPORT __attribute__((__visibility__("default")))
-        #define LIB_API_IMPORT __attribute__((__visibility__("default")))
-	 #else
-        #define LIB_API_EXPORT
-        #define LIB_API_IMPORT
-	 #endif
+	#define LIB_API_EXPORT
+	#define LIB_API_IMPORT
 #endif
 
 #if defined(LIB_EXPORTS)
