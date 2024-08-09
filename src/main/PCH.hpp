69ec00a8c45c4591d1ebf34b547f90108f8688bd
Andrew King <pepsiotaku@gmail.com>
2024-08-09 08:50:19 -0400

    add include to distribution, take PCH out of lib's build

--- src/main/PCH.hpp
+++ src/main/PCH.hpp
@@ -1,7 +1,17 @@
 #ifndef MAIN_PRECOMPILED_HEADER_HPP
 #define MAIN_PRECOMPILED_HEADER_HPP
 
-#include <sharedlib/PCH.hpp>
+#include <algorithm>
+#include <cstdio>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <vector>
 
 #endif // MAIN_PRECOMPILED_HEADER_HPP
 
