ecb3c05964d163a742702625c776f5ddbcdc9e44
Andrew King <pepsiotaku@gmail.com>
2025-10-08 21:41:46 -0400

    restore previous PCH structure to fix msvc warning

--- src/main/PCH.hpp
+++ src/main/PCH.hpp
@@ -1,17 +1,7 @@
 #ifndef MAIN_PRECOMPILED_HEADER_HPP
 #define MAIN_PRECOMPILED_HEADER_HPP
 
-#include <algorithm>
-#include <cstdio>
-#include <deque>
-#include <fstream>
-#include <iostream>
-#include <list>
-#include <map>
-#include <memory>
-#include <set>
-#include <string>
-#include <vector>
+#include <sharedlib/PCH.hpp>
 
 #endif // MAIN_PRECOMPILED_HEADER_HPP
 
